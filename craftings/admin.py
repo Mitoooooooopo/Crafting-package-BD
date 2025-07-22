166dcd2c3e3ac0a163d5198982cdf39d40dea14c
noobdog667 <106137571+Mitoooooooopo@users.noreply.github.com>
2025-07-22 11:53:07 +0530

    Update admin.py

--- craftings/admin.py
+++ craftings/admin.py
@@ -1,7 +1,7 @@
 from typing import TYPE_CHECKING, Any
 
 from django.contrib import admin
-from .models import CraftingRecipe, CraftingIngredient
+from .models import CraftingRecipe, CraftingIngredient, CraftingIngredientGroup, CraftingGroupOption
 from django.utils.safestring import mark_safe
 
 class CraftingIngredientInline(admin.TabularInline):
@@ -9,11 +9,28 @@ class CraftingIngredientInline(admin.TabularInline):
     extra = 1
     autocomplete_fields = ("ingredient",)
     fields = ("ingredient", "quantity") 
+
+class CraftingGroupOptionInline(admin.TabularInline):
+    model = CraftingGroupOption
+    extra = 1
+    autocomplete_fields = ("ball",)
+    fields = ("ball",)
+    
+class CraftingIngredientGroupInline(admin.StackedInline):
+    model = CraftingIngredientGroup
+    extra = 1
+    fields = ("name", "required_count")
+    show_change_link = True   
     
 @admin.register(CraftingRecipe)
 class CraftingRecipeAdmin(admin.ModelAdmin):
     list_display = ("result",)
-    inlines = [CraftingIngredientInline]
+    inlines = [CraftingIngredientInline, CraftingIngredientGroupInline]
     search_fields = ("result__name",)
-    autocomplete_fields = ("result",) 
-    
+    autocomplete_fields = ("result",)  
+
+@admin.register(CraftingIngredientGroup)
+class CraftingIngredientGroupAdmin(admin.ModelAdmin):
+    list_display = ("name", "required_count", "recipe")
+    inlines = [CraftingGroupOptionInline]
+    search_fields = ("ball__name", "group__name") 
